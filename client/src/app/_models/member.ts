84f08de9472efe5c0be82a12270d2dffaa1ace55
narendra7893 <icreatenaren@gmail.com>
2023-02-28 13:50:23 +0530

    Update code

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,20 @@
+import { photo } from "./photo";
+
+    export interface member {
+        id: number;
+        userName: string;
+        photoUrl: string;
+        age: number;
+        knownAs: string;
+        created: Date;
+        lastActive: Date;
+        gender: string;
+        introduction: string;
+        lookingFor: string;
+        interests: string;
+        city: string;
+        country: string;
+        photos: photo[];
+    }
+
+
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { User } from './_models/User';
+import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 
 @Component({
@@ -11,20 +11,11 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit {
   title = 'Datting App Running';
   users:any;
-  constructor(private http: HttpClient, private accountService: AccountService){}
+  constructor(private accountService: AccountService){}
   ngOnInit() : void{
-this.getUsers();
 this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next : response => this.users = response,
-      error : error => console.log(error),
-      complete : () => console.log("Request has completed")
-    })
-
-  }
 
   setCurrentUser(){
     const userString = localStorage.getItem('user');
